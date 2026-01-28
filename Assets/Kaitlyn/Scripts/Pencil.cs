3e1ee753b0784a46296af6a7464b9c93dd52ad7e
MUKILTEO\1725577 <kbw2205@icloud.com>
2026-01-28 13:38:20 -0800

    wir sind pivoting :skull:
    pencils spawn but they dont move
    
    i need to accurately grab direction and stuff
    
    gonna change my script to utilize alexs suggestion

--- Assets/Kaitlyn/Scripts/Pencil.cs
+++ Assets/Kaitlyn/Scripts/Pencil.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Pencil : MonoBehaviour
 {
@@ -17,8 +18,10 @@ public class Pencil : MonoBehaviour
         
     }
 
-    public void PencilToSpawn()
+    public void PencilToSpawn(InputAction.CallbackContext ctx)
     {
+        if(!ctx.performed) return;
+
         PencilData pencil = pencils[Random.Range(0, pencils.Count)];
 
         SpawnPencil(pencil);
@@ -26,23 +29,26 @@ public class Pencil : MonoBehaviour
 
     public void SpawnPencil(PencilData penkil)
     {
-        
+        Vector3 startPos = penkil.pencilSpawnPoint.position;
+        Vector3 endPos = -penkil.pencilSpawnPoint.position;
+
         float randomAngle = Random.Range(-penkil.angleRange, penkil.angleRange);
 
         Quaternion rotation = Quaternion.Euler(0, 0, randomAngle);
-        Vector3 spawnDirection = rotation * Vector3.forward;
+        Vector3 spawnDirection = rotation * Vector3.up;
 
         GameObject pencil = Instantiate(pencilPrefab, penkil.pencilSpawnPoint.position, Quaternion.LookRotation(spawnDirection));
 
-        Rigidbody rb = pencil.GetComponent<Rigidbody>();
+        //Rigidbody2D rb = pencil.GetComponent<Rigidbody2D>();
 
-        if(rb != null)
+        //if(rb != null)
         {
-            rb.linearVelocity = spawnDirection * penkil.pencilSpeed;
+            //rb.linearVelocity = spawnDirection * penkil.pencilSpeed;
         }
     }
 }
 
+[System.Serializable]
 public struct PencilData
 {
     public Transform pencilSpawnPoint;
