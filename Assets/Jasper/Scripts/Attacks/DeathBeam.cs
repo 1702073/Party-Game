81b6527d3dfccfc8575552784d5e5683ce5223cd
MUKILTEO\1725659 <jasper.aleksander.nyman@gmail.com>
2026-02-03 13:24:48 -0800

    DEATH BEAM FUNCTIONALLY ALMOST DONE

--- Assets/Jasper/Scripts/Attacks/DeathBeam.cs
+++ Assets/Jasper/Scripts/Attacks/DeathBeam.cs
@@ -2,15 +2,70 @@ using UnityEngine;
 
 public class DeathBeam : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // References
+    [Header("References")]
+    public GameObject warning;
+    public GameObject beam;
+
+    // Configuration
+    [Header("General Configuration")]
+    public float width;
+
+    [Header("Warning Configuration")]
+    public float warningDuration;
+    public Color warningColor;
+
+    [Header("Beam Configuration")]
+    public float beamDuration;
+    public Color beamColor;
+
     void Start()
     {
-        
+        warning.GetComponent<SpriteRenderer>().color = warningColor;
+        warning.transform.localScale = new Vector3(
+            width,
+            warning.transform.localScale.y,
+            warning.transform.localScale.z
+        );
+
+        beam.GetComponent<SpriteRenderer>().color = beamColor;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (warningDuration > 0) {
+            warningDuration -= Time.deltaTime;
+        } else if (!beam.activeSelf) {
+            warning.SetActive(false);
+            Debug.Log("warning no longer exists");
+            beam.SetActive(true);
+            Debug.Log("BEAM EXISTS NOW AAAHAHAHAHAHAHAHAAAAA");
+        }
+
+        // DEATH BEAM AAAHAHAHAHAHAHAHAAAAA
+        if (beam.activeSelf) {
+            if (beamDuration > 0) {
+                // Enlarge death beam to set width
+                if (beam.transform.localScale.x < width) { 
+                    beam.transform.localScale += new Vector3(Time.deltaTime * (5 * width), 0, 0);
+                }
+
+                beamDuration -= Time.deltaTime;
+            } else {
+                // Shrink death beam before self destruction
+                if (beam.transform.localScale.x > 0) { 
+                    beam.transform.localScale -= new Vector3(Time.deltaTime * (5 * width), 0, 0);
+                }
+                else {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        beam.transform.localScale = new Vector3(
+            Mathf.Clamp(beam.transform.localScale.x, 0, width),
+            beam.transform.localScale.y,
+            beam.transform.localScale.z
+        );
     }
 }
